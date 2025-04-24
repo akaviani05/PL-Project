6e99ad9eda3f5b432c5b32084851387e5275ff55
Soheil Mohammadkhani <mohammadkhani.soheil@gmail.com>
2025-04-24 16:11:38 +0330

    new test

--- lexer-tester.rkt
+++ lexer-tester.rkt
@@ -1,30 +1,45 @@
 #lang racket
 
-(require parser-tools/lex) ; Required for token-name
-(require "lexer.rkt")     ; Your lexer file
+(require parser-tools/lex)
+(require "lexer.rkt") 
 
-(define sample-code
+(define sample-code1
   "
-  3 * 4 + 5 * 6 / 7 + 2
+  int a;
+  int b;
+  int c;
+  a = $input() + 5;
+  b = a + (b * b * b);
+
+  int add(int a, int b) {
+    return a + b;
+  }
+
+  c = add(a, b) + 2;
+  if (c > 10) {
+    $print("hello there");
+  } else {
+    $print("goodbye");
+  }
+
+  while (c < 20) {
+    c = c + 1;
+  }
   ")
 
-; --- Lexing the input string ---
 (define in (open-input-string sample-code))
-(define tokens '()) ; List to store the tokens
+(define tokens '())
 
-; Loop to get all tokens until EOF
 (let loop ()
   (define current-token (full-lexer in))
   (set! tokens (cons current-token tokens))
-  ; Stop when the EOF token is encountered
   (unless (eq? (token-name current-token) 'EOF)
     (loop)))
 
 (close-input-port in)
-(define final-token-list (reverse tokens)) ; Reverse to get the correct order
+(define final-token-list (reverse tokens)) 
 
-; --- Displaying the results ---
 (displayln "Input String:")
 (displayln sample-code)
 (displayln "\nLexed Tokens:")
-(pretty-print final-token-list) ; Use pretty-print for better formatting
+(pretty-print final-token-list)
